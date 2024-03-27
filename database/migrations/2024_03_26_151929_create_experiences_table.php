c50645eea22dc05cc9f3f4bc934f40231cef6d71
PFvdB121 <patriquevdboom@gmail.com>
2024-03-27 21:44:17 +0100

    Added experience table

--- database/migrations/2024_03_26_151929_create_experiences_table.php
+++ database/migrations/2024_03_26_151929_create_experiences_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('experiences', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained("users");
+            $table->string("Where");
+            $table->date("Begin");
+            $table->date("Ended");
             $table->timestamps();
         });
     }
