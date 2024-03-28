d8cd5087a92d0568cf22bcd8de1887e526683ae1
PFvdB121 <patriquevdboom@gmail.com>
2024-03-28 10:52:22 +0100

    Added day of birth and password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string("username")->unique();
             $table->string('email')->unique();
             $table->integer("phone_number");
-            $table->date("birth");
+            $table->date("day_of_birth");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
